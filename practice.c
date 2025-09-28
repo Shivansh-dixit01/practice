2f999bade0e24010fb61a24c9d47431b5db81d77
Shivansh-dixit01 <shivansh.13dixit@gmail.com>
2025-09-28 13:17:38 +0530

    first commit

---
+++ practice.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+    {
+        int a,b;
+        printf("put the valueof a : ");
+        scanf("%d",&a);
+        printf("put the the value of b : ");
+        scanf("%d",&b);
+        int c = a%b;
+        printf("%d",c);
+        return 0;
+    }
